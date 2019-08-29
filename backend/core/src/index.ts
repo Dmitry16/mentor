752e55f0db17b82bd210c508b58f12f23f116b41
Dmitry16 <europartner@inbox.ru>
2019-08-29 13:19:24 +0300

    be:server

--- backend/core/src/index.ts
+++ backend/core/src/index.ts
@@ -1,1 +1,12 @@
-console.log('aloha from typescript!')
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.end('kuku from mentor!');
+})
+
+app.listen(3003, () => console.log('server is listening on port 3003'));
